e8b47978130bd6f3c379c78557f7dc303a7f86f5
Jessica Frazelle <acidburn@docker.com>
2015-10-04 20:16:19 -0700

    cleanup pastebinit server
    Signed-off-by: Jessica Frazelle <acidburn@docker.com>

--- main.go
+++ main.go
@@ -6,63 +6,111 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 	"strings"
+
+	"github.com/Sirupsen/logrus"
 )
 
 var (
-	args     []string
-	content  []byte
-	baseuri  string
-	username string = os.Getenv("PASTEBINIT_USERNAME")
-	pass     string = os.Getenv("PASTEBINIT_PASS")
+	baseuri string
+
+	username = os.Getenv("PASTEBINIT_USERNAME")
+	password = os.Getenv("PASTEBINIT_PASS")
 )
+
+// readFromStdin returns everything in stdin
+func readFromStdin() []byte {
+	stdin, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		logrus.Fatalf("reading from stdin failed: %v", err)
+	}
+	return stdin
+}
+
+// readFromFile returns the contents of a file
+func readFromFile(filename string) []byte {
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		logrus.Fatalf("No such file or directory: %q", filename)
+	}
+
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+		logrus.Fatalf("reading from file %q failed: %v", filename, err)
+	}
+	return file
+}
+
+// postPaste uploads the paste content to the server
+// and returns the paste URI
+func postPaste(content []byte) (string, error) {
+	// create the request
+	req, err := http.NewRequest("POST", baseuri+"paste", bytes.NewBuffer(content))
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.SetBasicAuth(username, password)
+
+	// do the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("request to %spaste failed: %v", baseuri, err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == 401 {
+		return "", fmt.Errorf("Unauthorized. Please check your username and pass. %d", resp.StatusCode)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("reading response body failed: %v", err)
+	}
+
+	var response map[string]string
+	if err = json.Unmarshal(body, &response); err != nil {
+		return "", fmt.Errorf("parsing body as json failed: %v", err)
+	}
+
+	if respError, ok := response["error"]; ok {
+		return "", fmt.Errorf("server responded with %s", respError)
+	}
+
+	pasteURI, ok := response["uri"]
+	if !ok {
+		return "", fmt.Errorf("what the hell did we get back even? %s", string(body))
+	}
+
+	return pasteURI, nil
+}
 
 func init() {
 	flag.StringVar(&baseuri, "b", "https://paste.j3ss.co/", "pastebin base url")
 	flag.Parse()
 
-	args = flag.Args()
-
 	// make sure uri ends with trailing /
 	if !strings.HasSuffix(baseuri, "/") {
 		baseuri += "/"
 	}
-
 	// make sure it starts with http(s)://
 	if !strings.HasPrefix(baseuri, "http") {
 		baseuri = "http://" + baseuri
 	}
 
-	if username == "" || pass == "" {
-		log.Fatalf("you need to pass the PASTEBINIT_USERNAME and PASTEBINIT_PASS env variables")
+	// make sure we have a username and password
+	if username == "" || password == "" {
+		logrus.Fatalf("you need to pass the PASTEBINIT_USERNAME and PASTEBINIT_PASS env variables")
 	}
-}
-
-func readFromStdin() []byte {
-	stdin, err := ioutil.ReadAll(os.Stdin)
-	if err != nil {
-		log.Fatalf("reading from stdin failed: %v", err)
-	}
-	return stdin
-}
-
-func readFromFile(filename string) []byte {
-	if _, err := os.Stat(filename); os.IsNotExist(err) {
-		log.Fatalf("No such file or directory: %q", filename)
-	}
-
-	file, err := ioutil.ReadFile(filename)
-	if err != nil {
-		log.Fatalf("reading from file %q failed: %v", filename, err)
-	}
-	return file
 }
 
 func main() {
+	args := flag.Args()
+
 	// check if we are reading from a file or stdin
+	var content []byte
 	if len(args) == 0 {
 		content = readFromStdin()
 	} else {
@@ -70,45 +118,10 @@ func main() {
 		content = readFromFile(filename)
 	}
 
-	// create the request
-	req, err := http.NewRequest("POST", baseuri+"paste", bytes.NewBuffer(content))
+	pasteURI, err := postPaste(content)
 	if err != nil {
-		log.Fatalf("creating new http request failed: %v", err)
-	}
-	req.Header.Set("Content-Type", "application/json")
-	req.SetBasicAuth(username, pass)
-
-	// do the request
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatalf("request to %spaste failed: %v", baseuri, err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode == 401 {
-		log.Fatal("Unauthorized. Please check your username and pass.")
+		logrus.Fatal(err)
 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalf("reading response body failed: %v", err)
-	}
-
-	var response map[string]string
-	if err = json.Unmarshal(body, &response); err != nil {
-		log.Fatalf("parsing body as json failed: %v", err)
-	}
-
-	if respError, ok := response["error"]; ok {
-		log.Fatalf("server responded with %s", respError)
-	}
-
-	var pasteUri string
-	var ok bool
-	if pasteUri, ok = response["uri"]; !ok {
-		log.Fatalf("what the hell did we get back even? %s", string(body))
-	}
-
-	fmt.Printf("Your paste has been uploaded here:\n%s\nthe raw object is here: %s/raw", pasteUri, pasteUri)
+	fmt.Printf("Your paste has been uploaded here:\n%s\nthe raw object is here: %s/raw", pasteURI, pasteURI)
 }
--- server/server.go
+++ server/server.go
@@ -2,20 +2,17 @@ package main
 
 import (
 	"crypto/rand"
-	"encoding/base64"
 	"encoding/json"
-	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
-	"path"
 	"path/filepath"
 	"strings"
 
+	"github.com/Sirupsen/logrus"
 	"github.com/sourcegraph/syntaxhighlight"
 )
 
@@ -38,33 +35,17 @@ var (
 	storage  string
 	certFile string
 	keyFile  string
-	username string = os.Getenv("PASTEBINIT_USERNAME")
-	pass     string = os.Getenv("PASTEBINIT_PASS")
 
-	chars = []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")
+	username = os.Getenv("PASTEBINIT_USERNAME")
+	password = os.Getenv("PASTEBINIT_PASS")
 )
 
-func init() {
-	flag.StringVar(&baseuri, "b", "https://paste.j3ss.co/", "url base for this domain")
-	flag.StringVar(&port, "p", "8080", "port for server to run on")
-	flag.StringVar(&storage, "s", "files", "directory to store pastes")
-	flag.StringVar(&certFile, "-cert", "", "path to ssl certificate")
-	flag.StringVar(&keyFile, "-key", "", "path to ssl key")
-
-	flag.Parse()
-
-	if username == "" || pass == "" {
-		log.Fatalf("you need to pass the PASTEBINIT_USERNAME and PASTEBINIT_PASS env variables")
-	}
-
-	// ensure uri has trailing slash
-	if !strings.HasSuffix(baseuri, "/") {
-		baseuri += "/"
-	}
-}
-
+// JSONResponse is a map[string]string
+// response from the web server
 type JSONResponse map[string]string
 
+// String returns the string representation of the
+// JSONResponse object
 func (j JSONResponse) String() string {
 	str, err := json.MarshalIndent(j, "", "  ")
 	if err != nil {
@@ -76,43 +57,169 @@ func (j JSONResponse) String() string {
 	return string(str)
 }
 
-func auth(r *http.Request) bool {
-	if _, ok := r.Header["Authorization"]; !ok {
-		log.Print("no auth header found")
-		return false
-	}
-	auth := strings.SplitN(r.Header["Authorization"][0], " ", 2)
+// generateIndexHTML generates the html for the index page
+// to list all the pastes, via walking the storage directory
+func generateIndexHTML() (string, error) {
+	var files string
 
-	if len(auth) != 2 || auth[0] != "Basic" {
-		log.Print("bad auth syntax")
-		return false
-	}
-
-	payload, err := base64.StdEncoding.DecodeString(auth[1])
-	if err != nil {
-		log.Printf("decoding auth string failed: %v", err)
-		return false
-	}
-	pair := strings.SplitN(string(payload), ":", 2)
-
-	if len(pair) != 2 || pair[0] != username || pair[1] != pass {
-		log.Printf("someone tried and failed to get in")
-		return false
+	// create the function to walk the pastes files
+	walkPastes := func(pth string, f os.FileInfo, err error) error {
+		base := filepath.Base(pth)
+		if base != storage {
+			files += fmt.Sprintf(`<tr>
+<td><a href="%s%s">%s</a></td>
+<td>%s</td>
+<td>%d</td>
+</tr>`, baseuri, base, base, f.ModTime().Format("2006-01-02T15:04:05Z07:00"), f.Size())
+		}
+		return nil
 	}
 
-	return true
+	// walk the pastes
+	if err := filepath.Walk(storage, walkPastes); err != nil {
+		return "", fmt.Errorf("walking %s failed: %v", storage, err)
+	}
+
+	html := fmt.Sprintf(`%s
+<table>
+	<thead>
+		<tr>
+			<th>name</th><th>modified</th><th>size</th>
+		</tr>
+	</thead>
+	<tbody>
+		%s
+	</tbody>
+</table>
+%s`, htmlBegin, files, htmlEnd)
+
+	return html, nil
 }
 
-func writeError(w http.ResponseWriter, msg string) {
-	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprint(w, JSONResponse{
-		"error": msg,
-	})
-	log.Printf("writing error: %s", msg)
+// pasteHandler is the request handler for / and /{pasteid}
+// it returns a list of all pastes to / if properly authed
+// and returns the paste to the public if /{pasteid} exists
+func pasteHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		// they want the root, make them auth
+		u, p, ok := r.BasicAuth()
+		if (u != username || p != password) || !ok {
+			w.Header().Set("WWW-Authenticate", `Basic realm="`+baseuri+`"`)
+			w.WriteHeader(401)
+			w.Write([]byte("401 Unauthorized\n"))
+			return
+		}
+
+		html, err := generateIndexHTML()
+		if err != nil {
+			writeError(w, fmt.Sprintf("generating index html failed: %v", err))
+			return
+		}
+
+		// write the html
+		w.Header().Set("Content-Type", "text/html")
+		fmt.Fprint(w, html)
+		logrus.Info("index file rendered")
+		return
+	}
+
+	filename := filepath.Join(storage, strings.Trim(r.URL.Path, "/"))
+	raw := strings.HasSuffix(filename, "/raw")
+
+	if raw {
+		// if they want the raw file serve a text/plain Content-Type
+		w.Header().Set("Content-Type", "text/plain")
+		// trim '/raw' from the filename so we can get the right file
+		filename = strings.TrimSuffix(filename, "/raw")
+	}
+
+	// check if the file exists
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		writeError(w, fmt.Sprintf("No such file or directory: %s", filename))
+		return
+	}
+
+	// read the file
+	src, err := ioutil.ReadFile(filename)
+	if err != nil {
+		writeError(w, fmt.Sprintf("Reading file %s failed: %v", filename, err))
+		return
+	}
+
+	if raw {
+		// serve the raw file
+		w.Write(src)
+		logrus.Printf("raw paste served: %s", filename)
+		return
+	}
+
+	// try to syntax highlight the file
+	highlighted, err := syntaxhighlight.AsHTML(src)
+	if err != nil {
+		writeError(w, fmt.Sprintf("Highlighting file %s failed: %v", filename, err))
+		return
+	}
+
+	// serve the highlighted file
+	fmt.Fprintf(w, "%s<pre><code>%s</code></pre>%s", htmlBegin, string(highlighted), htmlEnd)
+	logrus.Printf("highlighted paste served: %s", filename)
 	return
 }
 
+// pasteUploadHander is the request handler for /paste
+// it creates a uuid for the paste and saves the contents of
+// the paste to that file
+func pasteUploadHandler(w http.ResponseWriter, r *http.Request) {
+	// check basic auth
+	u, p, ok := r.BasicAuth()
+	if (u != username || p != password) || !ok {
+		w.Header().Set("WWW-Authenticate", `Basic realm="`+baseuri+`"`)
+		w.WriteHeader(401)
+		w.Write([]byte("401 Unauthorized\n"))
+		return
+	}
+
+	// set the content type and check to make sure they are POST-ing a paste
+	w.Header().Set("Content-Type", "application/json")
+	if r.Method != "POST" {
+		writeError(w, "not a valid endpoint")
+		return
+	}
+
+	// read the body of the paste
+	content, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		writeError(w, fmt.Sprintf("reading from body failed: %v", err))
+		return
+	}
+
+	// create a unique id for the paste
+	id, err := uuid()
+	if err != nil {
+		writeError(w, fmt.Sprintf("uuid generation failed: %v", err))
+		return
+	}
+
+	// write to file
+	file := filepath.Join(storage, id)
+	if err := ioutil.WriteFile(file, content, 0755); err != nil {
+		writeError(w, fmt.Sprintf("writing file to %q failed: %v", file, err))
+		return
+	}
+
+	// serve the uri for the paste to the requester
+	fmt.Fprint(w, JSONResponse{
+		"uri": baseuri + id,
+	})
+	logrus.Infof("paste %q posted successfully", id)
+	return
+}
+
+// uuid generates a uuid for the paste
+// this really does not need to be perfect
 func uuid() (string, error) {
+	var chars = []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")
+
 	length := 8
 	b := make([]byte, length)
 	r := make([]byte, length+(length/4))
@@ -134,155 +241,63 @@ func uuid() (string, error) {
 			}
 		}
 	}
+}
 
-	return "", errors.New("[uuid]: We should not have gotten here")
+// writeError sends an error back to the requester
+// and also logrus. the error
+func writeError(w http.ResponseWriter, msg string) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprint(w, JSONResponse{
+		"error": msg,
+	})
+	logrus.Printf("writing error: %s", msg)
+	return
+}
+
+func init() {
+	flag.StringVar(&baseuri, "b", "https://paste.j3ss.co/", "url base for this domain")
+	flag.StringVar(&port, "p", "8080", "port for server to run on")
+	flag.StringVar(&storage, "s", "/etc/pastebinit/files", "directory to store pastes")
+	flag.StringVar(&certFile, "-cert", "", "path to ssl certificate")
+	flag.StringVar(&keyFile, "-key", "", "path to ssl key")
+	flag.Parse()
+
+	if username == "" || password == "" {
+		logrus.Fatalf("you need to pass the PASTEBINIT_USERNAME and PASTEBINIT_PASS env variables")
+	}
+
+	// ensure uri has trailing slash
+	if !strings.HasSuffix(baseuri, "/") {
+		baseuri += "/"
+	}
 }
 
 func main() {
 	// create the storage directory
 	if err := os.MkdirAll(storage, 0755); err != nil {
-		log.Fatalf("creating storage directory %q failed: %v", storage, err)
+		logrus.Fatalf("creating storage directory %q failed: %v", storage, err)
 	}
 
 	// create mux server
 	mux := http.NewServeMux()
 
-	// static files
+	// static files handler
 	staticHandler := http.StripPrefix("/static/", http.FileServer(http.Dir("/src/static")))
 	mux.Handle("/static/", staticHandler)
 
-	// upload function
-	mux.HandleFunc("/paste", func(w http.ResponseWriter, r *http.Request) {
-		// make them auth
-		allowed := auth(r)
-		if !allowed {
-			w.Header().Set("WWW-Authenticate", `Basic realm="`+baseuri+`"`)
-			w.WriteHeader(401)
-			w.Write([]byte("401 Unauthorized\n"))
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		if r.Method != "POST" {
-			writeError(w, "not a valid endpoint")
-			return
-		}
-
-		content, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			writeError(w, fmt.Sprintf("reading from body failed: %v", err))
-			return
-		}
-
-		// create a unique id for the paste
-		id, err := uuid()
-		if err != nil {
-			writeError(w, fmt.Sprintf("uuid generation failed: %v", err))
-			return
-		}
-
-		// write to file
-		filepath := path.Join(storage, id)
-		if err = ioutil.WriteFile(filepath, content, 0755); err != nil {
-			writeError(w, fmt.Sprintf("writing file to %q failed: %v", filepath, err))
-			return
-		}
-
-		fmt.Fprint(w, JSONResponse{
-			"uri": baseuri + id,
-		})
-		log.Printf("paste posted successfully")
-		return
-	})
-
-	// index function
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html")
-		if r.URL.Path == "/" {
-			// they want the root
-			// if probably me the admin
-			// but make them auth
-			allowed := auth(r)
-			if !allowed {
-				w.Header().Set("WWW-Authenticate", `Basic realm="`+baseuri+`"`)
-				w.WriteHeader(401)
-				w.Write([]byte("401 Unauthorized\n"))
-				return
-			}
-
-			var files string
-			err := filepath.Walk(storage, func(path string, f os.FileInfo, err error) error {
-				if filepath.Base(path) != storage {
-					files += fmt.Sprintf(`<tr>
-<td><a href="%s%s">%s</a></td>
-<td>%s</td>
-<td>%d</td>
-</tr>`, baseuri, filepath.Base(path), filepath.Base(path), f.ModTime().Format("2006-01-02T15:04:05Z07:00"), f.Size())
-				}
-				return nil
-			})
-			if err != nil {
-				writeError(w, fmt.Sprintf("walking %s failed: %v", storage, err))
-			}
-
-			fmt.Fprintf(w, "%s<table><thead><tr><th>name</th><th>modified</th><th>size</th></tr></thead><tbody>%s</tbody></table>%s", htmlBegin, files, htmlEnd)
-			log.Printf("index file rendered")
-			return
-		}
-
-		// check if a file exists for what they are asking
-		serveRaw := false
-		filename := strings.Trim(r.URL.Path, "/")
-		filename = path.Join(storage, filename)
-
-		// check if they want the raw file
-		if strings.HasSuffix(filename, "/raw") {
-			w.Header().Set("Content-Type", "text/plain")
-			filename = strings.TrimSuffix(filename, "/raw")
-			serveRaw = true
-		}
-
-		// check if file exists
-		if _, err := os.Stat(filename); os.IsNotExist(err) {
-			writeError(w, fmt.Sprintf("No such file or directory: %q", filename))
-			return
-		}
-
-		// read the file
-		src, err := ioutil.ReadFile(filename)
-		if err != nil {
-			writeError(w, fmt.Sprintf("Reading file %q failed: %v", filename, err))
-			return
-		}
-
-		if serveRaw {
-			// serve the file
-			w.Write(src)
-			log.Printf("raw paste served: %s", src)
-			return
-		}
-
-		// try to syntax highlight the file
-		highlighted, err := syntaxhighlight.AsHTML(src)
-		if err != nil {
-			writeError(w, fmt.Sprintf("Highlighting file %q failed: %v", filename, err))
-			return
-		}
-
-		fmt.Fprintf(w, "%s<pre><code>%s</code></pre>%s", htmlBegin, string(highlighted), htmlEnd)
-		log.Printf("highlighted paste served: %s", filename)
-		return
-	})
+	// pastes & view handlers
+	mux.HandleFunc("/paste", pasteUploadHandler) // paste upload handler
+	mux.HandleFunc("/", pasteHandler)            // index & paste server handler
 
 	// set up the server
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
-	log.Printf("Starting server on port %q with baseuri %q", port, baseuri)
+	logrus.Infof("Starting server on port %q with baseuri %q", port, baseuri)
 	if certFile != "" && keyFile != "" {
-		log.Fatal(server.ListenAndServeTLS(certFile, keyFile))
+		logrus.Fatal(server.ListenAndServeTLS(certFile, keyFile))
 	} else {
-		log.Fatal(server.ListenAndServe())
+		logrus.Fatal(server.ListenAndServe())
 	}
 }
